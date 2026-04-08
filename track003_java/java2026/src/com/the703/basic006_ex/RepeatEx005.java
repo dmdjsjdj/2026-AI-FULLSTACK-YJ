6c9748c585c2028d2da350c337de6ce972b75b02
dmdjsjdj <jenia03s@gmail.com>
2026-04-08 21:07:05 +0900

    repeat005 수정

--- track003_java/java2026/src/com/the703/basic006_ex/RepeatEx005.java
+++ track003_java/java2026/src/com/the703/basic006_ex/RepeatEx005.java
@@ -10,28 +10,32 @@ public class RepeatEx005 {
 		//apple(x)
 		
 		String sum = " ";
-		int a = 0;
-		
+		boolean non = false;
 		Scanner scanner = new Scanner(System.in);
 		
 		System.out.print("문자열을 입력하시오 : ");
 		sum = scanner.next();
 
 		int count = sum.length();
-
-		System.out.println(count+","+a);
 		
-		for(int i=0; i <= count/2;i++,count--) {
-			if(sum.charAt(i) == sum.charAt(count-1)) {
-				a++;
-				System.out.println(i+","+a);
-			}
-			
+		for(int i=0; i < count/2;i++) {
+			System.out.println(sum.charAt(i));
+			System.out.println(sum.charAt(count-i-1));
+			if(sum.charAt(i) != sum.charAt(count-i-1)) {
+				non = false;
+			} 
+			else { non=true; }
+			break;
 		}
-		System.out.println(count+","+a);
-		if(a==(count%2==0?count:count<2?count+1:count-1)) {
-			System.out.println("회문입니다");
-		} else { System.out.println("회문이 아닙니다"); }
+		if(non==false) {
+			System.out.println("회문이 아닙니다.");
+		}
+		else if(non==true) { System.out.println("회문입니다"); }  
+		
+//		System.out.println(count+","+a);
+//		if(a==(count%2==0?count:count<2?count+1:count-1)) {
+//			System.out.println("회문입니다");
+//		} else { System.out.println("회문이 아닙니다"); }
 		
 //			if(sum.charAt(i) == sum.charAt(count-1)) {
 //							System.out.println("회문입니다");
