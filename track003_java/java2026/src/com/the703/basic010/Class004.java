d0f7ec2b230bf0b6bf7aec09bd3872262b7c4091
dmdjsjdj <jenia03s@gmail.com>
2026-04-22 14:35:10 +0900

    day020 자바스크립트 업로드

---
+++ track003_java/java2026/src/com/the703/basic010/Class004.java
@@ -0,0 +1,27 @@
+package com.the703.basic010;
+
+public class Class004 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
+
+/*************
+■ OOP
+1.  생성자   - new 연산자에 의해 호출 [초기화] 담당
+
+2. 기본생성자( 디폴트생성자 )
+- 모든클래스에 생성자가 반드시 존재
+- 생성자선언을 생략시 컴파일러가 자동으로 기본생성자를 추가
+- 개발자가 선언시 컴파일러가 자동생성  [ 취소 ]
+
+3. 생성자형식
+class A{
+   public    A(                 ){}   //기본생성자(디폴트생성자)
+   public    A(String name){}   //파라미터, 알규먼트가 있는 생성자. toStrig도 같이 쓰면 좋음
+   
+}
+1)  리턴값  ( x )
+2)  클래스명과  ( 동일(클래스명 ) 
+*************/
