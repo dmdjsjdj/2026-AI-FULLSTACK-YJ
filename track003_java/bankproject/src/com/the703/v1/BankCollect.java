0f2bf9a31ffa46e6d3ccb9caa71c4c7d6d0cfd0f
dmdjsjdj <jenia03s@gmail.com>
2026-05-10 23:14:50 +0900

    bank

--- track003_java/bankproject/src/com/the703/v1/BankCollect.java
+++ track003_java/bankproject/src/com/the703/v1/BankCollect.java
@@ -3,6 +3,7 @@ package com.the703.v1;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Scanner;
 
 //1. Dto 데이터전송목적
 class BankDto{
@@ -39,36 +40,165 @@ class BankDto{
 	}
 }
 
-class Bank1{
+class Bank{
 	List<BankDto>  users;   // 객체를 생성하는게 아니라 정보만 받을 목적
 	
-	public Bank1() { super(); }
-	public Bank1(List<BankDto> users) { super(); this.users = users; }
+	public Bank() { super(); }
+	public Bank(List<BankDto> users) { super(); this.users = users; }
+	
+	Scanner sc = new Scanner(System.in);
+	BankDto login = null;
 	
 	// 메뉴 - 안에 내용작성
-	public void menu() {}   
+	public void menu() {
+		System.out.print("\n -- bank menu --\r\n"
+				 + "\r\n"
+				 + "1.➕계좌 추가\r\n" 
+				 + "2.🔍계좌 조회\r\n" 
+				 + "3.💵입금\r\n" 
+				 + "4.💸 출금\r\n" 
+				 + "5.🗑️삭제\r\n" 
+				 + "6.🔧비번 수정\r\n" 
+				 + "9.종료\r\n"
+				 + "\r\n"+"👉 번호를 선택하세요: ");
+	}   
 	// 유저추가  (add)
 	public void add() {
 		//변수
+		String id; String pass; double balance;
 		//입력 - 사용자에게 정보입력받기
+		System.out.print("아이디: ");
+		id = sc.next();
+		System.out.print("비밀번호: ");
+		pass = sc.next();
+		do {
+			System.out.print("잔액: ");
+			balance = sc.nextInt();
+		}while(balance<0);
 		//처리 
-		users.add( new BankDto("aaa" , "pass" , 1 ) );
+		users.add( new BankDto(id , pass , balance ) );
 		//출력
+		System.out.println(users);
 	}
+	// 사용자 확인
+	public void usercheck() {
+		String id2;	String pw2 ;  login = null;
+		// 아이디 비교    비밀번호 비교
+		System.out.print("아이디: ");
+		id2 = sc.next();
+		System.out.print("비밀번호: ");
+		pw2 = sc.next();
+		
+		for(int i=0;i<users.size();i++) {
+			if( users.get(i).getId().equals(id2) && users.get(i).getPass().equals(pw2)) {
+				login = users.get(i);
+				break;
+			}
+			//  if( id.equals(id2) || pw.equals(pw2)) { continue; }
+			else if( !users.get(i).getId().equals(id2) && !users.get(i).getPass().equals(pw2)) {
+				
+			}
+		}
+	}
+	// 잔액 조회
+	public void jan() {
+		System.out.println("잔액: "+ login.getBalance() + "원");
+	}                                                                                                                                              
 	// 입금   (get)
+	public void deposit() {
+		
+	}
 	// 출금   (get)
+	public void withdraw() {
+		
+	}
 	// 유저삭제(remove)
+	public void delete() {
+		
+	}
 	// 종료   
 }
 public class BankCollect {
 	public static void main(String[] args) {
 		List<BankDto>  users = new ArrayList<>();
-		Bank1      controller = new Bank1(users);
-		controller.menu();
+		Bank      controller = new Bank(users);
+		int menu=-1;	Scanner sc = new Scanner(System.in);
+		System.out.print("\n 🌟💰 welcome to bank 💰🌟\r\n");
 		
-		//테스트용
-		//		controller.add();
-		//		System.out.println(controller.users);
+		while( menu!=9 ) {
+			controller.menu();
+			menu = sc.nextInt();
+			if(menu==9) {  //9.종료 >> 종료합니다.
+				System.out.println("종료합니다.");
+			} 
+			else if (menu==1) { //1.추가 - 아이디 id, 비번 pw, 잔액 balance
+				controller.add();
+			} 
+			
+			else if (menu>=2 && menu<=6) {  
+					controller.usercheck();
+					if( controller.login == null ) {
+						
+					}
+					else {			
+						switch(menu) {
+						case 2: controller.jan(); break;
+							    
+						
+						case 3: System.out.print("입금금액: "); 
+								inputm = sc.nextInt();
+								
+								if(inputm>=0) {
+									balance[i] = balance[i] + (double)inputm;
+									System.out.println("잔액: "+ balance[i] + "원");
+								}
+								else { 
+										System.out.println("음수는 입력할 수 없습니다"); 
+								} break;
+								
+						case 4: System.out.print("출금금액: "); 
+								outputm = sc.nextInt();
+								
+								if(outputm>=0 && balance[i]-(double)outputm>=0) {
+									balance[i] = balance[i] - (double)outputm;
+									System.out.println("잔액: "+ balance[i] + "원");
+								}
+								else { 
+										System.out.println("잔액이 부족합니다.  현재 잔액"+balance[i]); 
+								} break;
+								
+						case 5: System.out.print("계좌를 삭제하시겠습니까?: ( y / n ) "); 
+								deletid = sc.next().charAt(0);
+								
+								if(deletid == 'y') {
+									System.out.println("계좌가 삭제되었습니다.");
+									id[i] = null;
+									pw[i] = null;
+								} else if(deletid == 'n') {
+									System.out.println("계좌 삭제를 취소하셨습니다.");
+								   } break;
+						
+						case 6: System.out.print("변경할 비밀번호: ");
+								pw2 = sc.next();
+								
+								pw[i] = pw2;
+								
+								break;
+								
+						
+						
+						default: System.out.println("잘못된 숫자입력입니다."); break;
+						};	 
+					}break;
+					
+				}
+				
+				
+			}
+			else {
+					System.out.println("\n 잘못된 입력입니다.");
+				}
+		}
 	}
 }
 
