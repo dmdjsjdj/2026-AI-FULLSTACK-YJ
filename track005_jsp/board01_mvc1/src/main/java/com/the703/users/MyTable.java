c66753bbffc6b734b9aa05b3a6d6921e85a4c566
dmdjsjdj <jenia03s@gmail.com>
2026-05-29 18:17:03 +0900

    day043

--- track005_jsp/board01_mvc1/src/main/java/com/the703/users/MyTable.java
+++ track005_jsp/board01_mvc1/src/main/java/com/the703/users/MyTable.java
@@ -14,7 +14,7 @@ public class MyTable extends HttpServlet {
     public MyTable() { super(); }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		// users 참고
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
